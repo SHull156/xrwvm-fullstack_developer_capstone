565ae44f4f2e1f191ad0868e83cf10242fc48bcb
SHull156 <sarahthull@gmail.com>
2025-09-09 13:18:26 -0400

    Build CarModel and CarMake Django Models

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,8 +28,17 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'https://sarahhull-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = ['https://sarahhull-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'https://sarahhull-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',  # old URL
+    'sarahhull-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',        # new URL
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://sarahhull-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',  # old URL
+    'https://sarahhull-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai', # new URL
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
