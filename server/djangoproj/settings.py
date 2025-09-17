8819341140749545100119b8d93c43788acd9d9c
SHull156 <sarahthull@gmail.com>
2025-09-17 17:21:14 +0100

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -8,18 +8,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = (
     'django-insecure-ccow$tz_=9%dxu4(0%^(z%nx32#s@(zt9$ih@)5l54yny)wm-0'
 )
-DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    'https://sarahhull-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',  # old URL
-    'https://sarahhull-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',  # new URL
+    'https://sarahhull-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'https://sarahhull-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://sarahhull-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',  # old URL
-    'https://sarahhull-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',  # new URL
+    'https://sarahhull-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'https://sarahhull-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
 ]
 
 REST_FRAMEWORK = {
